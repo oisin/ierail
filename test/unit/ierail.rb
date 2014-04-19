cbeb5d7b07fef0523bbfe3ef705053caf55a34e1
Gary Rafferty <gary.rafferty@gmail.com>
2014-04-19 20:01:53 +0100

    Take DST into account when it comes to timezones

--- test/unit/ierail.rb
+++ test/unit/ierail.rb
@@ -14,13 +14,17 @@ class IERailTest < MiniTest::Unit::TestCase
 
   def test_that_the_train_directions_are_correct
     VCR.use_cassette('northbound') do |cassette|
-      Timecop.freeze(cassette.originally_recorded_at || Time.now) do
+      time = get_original_time(cassette.originally_recorded_at)
+      
+      Timecop.freeze(time || Time.now) do
         northbound_train = @ir.northbound_from('Dublin Connolly').sample
         assert_equal northbound_train.direction, 'Northbound'
       end
     end
     VCR.use_cassette('northbound') do |cassette|
-      Timecop.freeze(cassette.originally_recorded_at || Time.now) do
+      time = get_original_time(cassette.originally_recorded_at)
+      
+      Timecop.freeze(time || Time.now) do
         southbound_train = @ir.southbound_from('Dublin Connolly').sample
         assert_equal southbound_train.direction, 'Southbound'
       end
@@ -29,7 +33,9 @@ class IERailTest < MiniTest::Unit::TestCase
 
   def test_that_an_empty_array_is_returned_when_no_data
     VCR.use_cassette('westbound') do |cassette|
-      Timecop.freeze(cassette.originally_recorded_at || Time.now) do
+      time = get_original_time(cassette.originally_recorded_at)
+      
+      Timecop.freeze(time || Time.now) do
         nonexistant = @ir.westbound_from('Clongriffin')
         assert_empty nonexistant
       end
@@ -38,7 +44,9 @@ class IERailTest < MiniTest::Unit::TestCase
 
   def test_that_the_before_time_constraint_works
     VCR.use_cassette('northbound') do |cassette|
-      Timecop.freeze(cassette.originally_recorded_at || Time.now) do
+      time = get_original_time(cassette.originally_recorded_at)
+
+      Timecop.freeze(time || Time.now) do
         #Thirty minutes from now
         thirty_mins = Time.now + (60 * 30)
         time = "#{thirty_mins.hour}:#{thirty_mins.min}" # "HH:MM"
@@ -50,7 +58,9 @@ class IERailTest < MiniTest::Unit::TestCase
 
   def test_that_the_after_time_constraint_works
     VCR.use_cassette('southbound') do |cassette|
-      Timecop.freeze(cassette.originally_recorded_at || Time.now) do
+      time = get_original_time(cassette.originally_recorded_at)
+      
+      Timecop.freeze(time || Time.now) do
         #Thirty minutes from now
         thirty_mins = Time.now + (60 * 30)
         time = "#{thirty_mins.hour}:#{thirty_mins.min}" # "HH:MM"
@@ -62,7 +72,9 @@ class IERailTest < MiniTest::Unit::TestCase
 
   def test_that_the_in_constraint_works
     VCR.use_cassette('southbound_from') do |cassette|
-      Timecop.freeze(cassette.originally_recorded_at || Time.now) do
+      time = get_original_time(cassette.originally_recorded_at)
+
+      Timecop.freeze(time || Time.now) do
         mins = 30
         thirty_mins = Time.now + (60 * mins)
         time = "#{thirty_mins.hour}:#{thirty_mins.min}" # "HH:MM"
@@ -81,7 +93,9 @@ class IERailTest < MiniTest::Unit::TestCase
 
   def test_station_times
     VCR.use_cassette('station_times') do |cassette|
-      Timecop.freeze(cassette.originally_recorded_at || Time.now) do
+      time = get_original_time(cassette.originally_recorded_at)
+      
+      Timecop.freeze(time || Time.now) do
         station_data = @ir.station_times('Dublin Connolly',30).sample
         assert_instance_of StationData, station_data
       end
@@ -90,7 +104,9 @@ class IERailTest < MiniTest::Unit::TestCase
 
   def test_find_station
     VCR.use_cassette('find_station') do |cassette|
-      Timecop.freeze(cassette.originally_recorded_at || Time.now) do
+      time = get_original_time(cassette.originally_recorded_at)
+      
+      Timecop.freeze(time || Time.now) do
         station = @ir.find_station('Dublin Connolly').sample
         assert_instance_of Struct::Station, station
       end
@@ -99,7 +115,9 @@ class IERailTest < MiniTest::Unit::TestCase
 
   def test_that_station_times_returns_station_data
     VCR.use_cassette('station_times') do |cassette|
-      Timecop.freeze(cassette.originally_recorded_at || Time.now) do
+      time = get_original_time(cassette.originally_recorded_at)
+      
+      Timecop.freeze(time || Time.now) do
         train = @ir.station_times('Dublin Connolly', 30).sample #random train in next 30 mins
         assert_equal train.class, StationData #StationData has already been tested
       end
