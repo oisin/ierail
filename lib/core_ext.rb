c6bae793d9f72936867945f6d4a993df76c3bb5b
Michael Dever <michael.dever2@mail.dcu.ie>
2014-04-19 17:40:27 +0100

    Tidy up filters

--- lib/core_ext.rb
+++ lib/core_ext.rb
@@ -2,21 +2,21 @@ class Array
   #Filters elements, collecting those
   #whose times are before _time_
   def before time
-    select {|t| t.arrival[:expected] <= Time.parse(time) }
+    query_time = Time.parse(time)
+    select {|t| t.arrival[:expected] <= query_time }
   end
 
   #Filters elements, collecting those
   #whose times are after _time_
   def after time
-    select {|t| t.arrival[:expected] >= Time.parse(time) }
+    query_time = Time.parse(time)
+    select {|t| t.arrival[:expected] >= query_time }
   end
 
   # The 'in' is just sugar really, saving the 
   # programmer from doing a trivial computation 
   # over and over again in their code.
   def in time
-    select { |t|
-      t.due_in <= time
-    }
+    select { |t| t.due_in <= time }
   end
 end
